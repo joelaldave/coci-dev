b87b2727c9be50cb25574c9ad6c562c6a0d5738f
joelaldave <aldavecueva@gmail.com>
2025-07-09 15:32:46 -0400

    Add initial auth routes and create auth page component with template

---
+++ src/app/feature/auth/auth.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+
+export const authRoutes: Routes = [
+    // {
+    //     path: '',
+    //     component: PrincipalLayoutComponent,
+    //     children: [
+    //         {
+    //             path: 'todo-list',
+    //             loadComponent: () => import('./todo-list/pages/general-page/general-page.component').then(m => m.GeneralPageComponent)
+    //         },
+    //     ]
+    // }
+]
---
+++ src/app/feature/auth/pages/auth-page/auth-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-page',
+  imports: [],
+  templateUrl: './auth-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AuthPageComponent { }
